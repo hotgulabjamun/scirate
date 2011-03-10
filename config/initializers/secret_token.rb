d8edfb6f57be7eb508eee480da568dc6df30e495
Dave Bacon <dabacon@gmail.com>
2011-03-10 13:12:33 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scirate::Application.config.secret_token = '8c25c90ff7f291830ab72892c1b3e1f91cc7f6152fa18a3e3b997cf7658150d8abb18e6d8b7cc81398ec799a9b2f86d3a8122142e03d56d4bcb77f122c15ee8f'
